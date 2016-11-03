cfd17ce262b7284982909ef6b5f02c5fdf63c5fa
Mike Dallas <mc-dallas@hotmail.com>
2016-11-03 02:02:23 +0000

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mockrequests',
-    version='0.1.0',
+    version='0.1.1',
     description='Easy unit testing for HTTP requests',
     url='https://github.com/mcdallas/mockrequests',
     author='Mike Dallas',
